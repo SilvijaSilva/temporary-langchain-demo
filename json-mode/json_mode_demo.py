af15f2e6d9b3135ea7d146be881294fa2ea7fb41
SilvijaSilva <silvija.klejevskyte@gmail.com>
2025-06-07 22:54:10 +0300

    json_mode_demo updated-working

--- json-mode/json_mode_demo.py
+++ json-mode/json_mode_demo.py
@@ -4,6 +4,7 @@ import os
 # Replace with your actual OpenAI API key
 # openai.api_key = "YOUR_OPENAI_API_KEY"
 from dotenv import load_dotenv
+from rich import print
 
 load_dotenv()  
 
@@ -11,19 +12,17 @@ token = os.getenv("MY_TOKEN")
 endpoint = "https://models.github.ai/inference"
 model = "openai/gpt-4.1-nano"
 
-client = ChatOpenAI(
-    base_url=endpoint,
+client = openai.OpenAI(
     api_key=token,
-    model=model,
-)
+    base_url=endpoint)
 
 response = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Once upon a time,"
-        }
-    ]
-)
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "What are three benefits of using Python for data analysis?"}
+            ],
+        temperature=0.7,
+    )
 
-print(response.choices[0].message.content.strip())
+print(response.choices[0].message.content)
